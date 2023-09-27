f835e1cefbe81c44176ff54bc3575b77db637961
alexmckme <mlexka@yahoo.fr>
2023-09-27 02:41:29 +0200

    feat: add local authentication strategy

--- routes/listingComptes.js
+++ routes/listingComptes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const listingComptesController = require("../controllers/listingComptes")
+const {ensureAuth} = require("../middleware/auth")
 
-router.get("/", listingComptesController.getComptes)
+router.get("/", ensureAuth, listingComptesController.getComptes)
 router.post("/addCompte", listingComptesController.addCompte)
 router.put("/markCompteComplete", listingComptesController.markCompteComplete)
 router.put("/markCompteUncomplete", listingComptesController.markCompteUncomplete)
